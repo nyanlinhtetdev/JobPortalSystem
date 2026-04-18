2b51d5ae442f86766350275677ad554301b67557
nyanlinhtetdev <nlinhtet.dev22@gmail.com>
2026-04-19 00:54:35 +0800

    feat: make DTOs for SaveJobs Controller

---
+++ JobPortalSystem/DTOs/SavedJobs/SaveJobRequestDto.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JobPortalSystem.Api.DTOs.SavedJobs
+{
+    public class SaveJobRequestDto
+    {
+        [Required]
+        public Guid JobId { get; set; }
+    }
+}
---
+++ JobPortalSystem/DTOs/SavedJobs/SavedJobDto.cs
@@ -0,0 +1,13 @@
+﻿namespace JobPortalSystem.Api.DTOs.SavedJobs
+{
+    public class SavedJobDto
+    {
+        public Guid Id { get; set; }
+        public Guid JobId { get; set; }
+        public DateTime? CreatedAt { get; set; }
+
+        public string Title { get; set; } = null!;
+        public string? Location { get; set; }
+        public int? Salary { get; set; }
+    }
+}
