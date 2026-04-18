2a7f11ad26d7b1aadb6cd1a06c8b8e9b686b1d6b
nyanlinhtetdev <nlinhtet.dev22@gmail.com>
2026-04-18 16:35:22 +0800

    feat: make interface for JobService

---
+++ JobPortalSystem/Services/Interfaces/IJobService.cs
@@ -0,0 +1,14 @@
+﻿using JobPortalSystem.Api.DTOs.Job;
+
+namespace JobPortalSystem.Api.Services.Interfaces
+{
+    public interface IJobService
+    {
+        Task<JobDto> CreateJob(JobRequestDto request, Guid employerId);
+        Task<JobDto?> GetJobPostById(Guid id);
+        Task<List<JobDto>> GetMyJobs(Guid id);
+        Task<int?> UpdateJob(Guid id, JobRequestDto request);
+        Task<int?> DeleteJob(Guid id);
+        Task<List<JobDto>> SearchJobs(JobSearchRequestDto request);
+    }
+}
