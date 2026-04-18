a9aa58d0807ca8f800ac4ab6c8de6f7ad6b91eac
nyanlinhtetdev <nlinhtet.dev22@gmail.com>
2026-04-18 16:29:02 +0800

    feat: make ApiResponse DTO that is used to return in every endpoint

---
+++ JobPortalSystem/DTOs/ApiResponse/ApiResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace JobPortalSystem.Api.DTOs.ApiResponse
+{
+    public class ApiResponse<T>
+    {
+        public bool Success { get; set; }
+        public T? Data { get; set; }
+        public string Message { get; set; } = "";
+        public object? Errors { get; set; }
+    }
+}
