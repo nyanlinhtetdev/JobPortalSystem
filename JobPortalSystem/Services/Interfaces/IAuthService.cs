e531b4754b6ea828d4bce19bb50323a27dc43e8f
nyanlinhtetdev <nlinhtet.dev22@gmail.com>
2026-04-18 16:32:27 +0800

    feat: make interface for AuthService

--- JobPortalSystem/Services/Interfaces/IAuthService.cs
+++ JobPortalSystem/Services/Interfaces/IAuthService.cs
@@ -1,10 +1,11 @@
-﻿using JobPortalSystem.Api.DTOs.Auth;
+﻿using JobPortalSystem.Api.DTOs.ApiResponse;
+using JobPortalSystem.Api.DTOs.Auth;
 
 namespace JobPortalSystem.Api.Services.Interfaces
 {
     public interface IAuthService
     {
-        Task<AuthResponseDto> Register(RegisterRequestDto request);
+        Task<ApiResponse<object>> Register(RegisterRequestDto request);
         Task<TokenResponseDto?> Login(LoginRequestDto request);
         Task<TokenResponseDto?> RefreshToken(RefreshTokenRequestDto request);
 
