8e97365315d39fe3de4ced9da392f5a3916c3b1c
nyanlinhtetdev <nlinhtet.dev22@gmail.com>
2026-04-18 18:45:38 +0800

    feat: add application repository

---
+++ JobPortalSystem/Repositories/ApplicationRepository.cs
@@ -0,0 +1,49 @@
+﻿using JobPortal.Database.AppDbContextModels;
+using JobPortalSystem.Api.DTOs.Applications;
+using JobPortalSystem.Api.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace JobPortalSystem.Api.Repositories
+{
+    public class ApplicationRepository : IApplicationRepository
+    {
+        private readonly AppDbContext _context;
+
+        public ApplicationRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<bool> JobExists(Guid jobId)
+        {
+            return await _context.Jobs.AnyAsync(j => j.Id == jobId && j.IsDeleted == false);
+        }
+
+        public async Task<bool> ApplicationExists(Guid jobId, Guid userId)
+        {
+            return await _context.JobApplications.AnyAsync(a => a.JobId == jobId && a.UserId == userId);
+        }
+
+        public async Task CreateApplication(JobApplication application)
+        {
+            await _context.JobApplications.AddAsync(application);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<List<JobApplicationDto>> GetApplicationsByUserId(Guid userId)
+        {
+            return await _context.JobApplications
+                .Where(a => a.UserId == userId)
+                .OrderByDescending(a => a.AppliedAt)
+                .AsNoTracking()
+                .Select(a => new JobApplicationDto
+                {
+                    Id = a.Id,
+                    JobId = a.JobId,
+                    Status = a.Status,
+                    AppliedAt = a.AppliedAt
+                })
+                .ToListAsync();
+        }
+    }
+}
---
+++ JobPortalSystem/Repositories/Interfaces/IApplicationRepository.cs
@@ -0,0 +1,13 @@
+﻿using JobPortal.Database.AppDbContextModels;
+using JobPortalSystem.Api.DTOs.Applications;
+
+namespace JobPortalSystem.Api.Repositories.Interfaces
+{
+    public interface IApplicationRepository
+    {
+        Task<bool> ApplicationExists(Guid jobId, Guid userId);
+        Task CreateApplication(JobApplication application);
+        Task<List<JobApplicationDto>> GetApplicationsByUserId(Guid userId);
+        Task<bool> JobExists(Guid jobId);
+    }
+}
