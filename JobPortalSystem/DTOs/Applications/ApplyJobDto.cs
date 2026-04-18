1b71ea4c3fc4c9c23ee6b2e09221a1f5773aa5f1
nyanlinhtetdev <nlinhtet.dev22@gmail.com>
2026-04-18 18:44:53 +0800

    feat: make required DTOs for Application Controller

---
+++ JobPortalSystem/DTOs/Applications/ApplyJobDto.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JobPortalSystem.Api.DTOs.Application
+{
+    public class ApplyJobDto
+    {
+        [Required]
+        public Guid JobId { get; set; }
+    }
+}
---
+++ JobPortalSystem/DTOs/Applications/JobApplicationDto.cs
@@ -0,0 +1,10 @@
+﻿namespace JobPortalSystem.Api.DTOs.Applications
+{
+    public class JobApplicationDto
+    {
+        public Guid Id { get; set; }
+        public Guid JobId { get; set; }
+        public string? Status { get; set; }
+        public DateTime? AppliedAt { get; set; }
+    }
+}
