594a4ec5d3b81c527fb1b695969fa2a10b4540ff
nyanlinhtetdev <nlinhtet.dev22@gmail.com>
2026-04-19 00:51:26 +0800

    feat: make DTOs for Application controller

---
+++ JobPortalSystem/DTOs/Applications/ApplicantDto.cs
@@ -0,0 +1,14 @@
+﻿namespace JobPortalSystem.Api.DTOs.Applications
+{
+    public class ApplicantDto
+    {
+        public Guid ApplicationId { get; set; }
+        public Guid JobId { get; set; }
+
+        public Guid UserId { get; set; }
+        public string Email { get; set; } = null!;
+
+        public string? Status { get; set; }
+        public DateTime? AppliedAt { get; set; }
+    }
+}
---
+++ JobPortalSystem/DTOs/Applications/UpdateStatusDto.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JobPortalSystem.Api.DTOs.Applications
+{
+    public class UpdateStatusDto
+    {
+        [Required]
+        [RegularExpression("^(Accept|Reject)$", ErrorMessage = "Status must be Accept or Reject.")]
+        public string Status { get; set; } = null!;
+    }
+}
