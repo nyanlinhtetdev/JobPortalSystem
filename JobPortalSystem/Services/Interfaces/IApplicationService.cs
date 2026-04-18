449c557c021f939e5196c38f030e898da1a55944
nyanlinhtetdev <nlinhtet.dev22@gmail.com>
2026-04-18 18:42:04 +0800

    feat: make interface for Application Service

---
+++ JobPortalSystem/Services/Interfaces/IApplicationService.cs
@@ -0,0 +1,11 @@
+﻿using JobPortalSystem.Api.DTOs.Application;
+using JobPortalSystem.Api.DTOs.Applications;
+
+namespace JobPortalSystem.Api.Services.Interfaces
+{
+    public interface IApplicationService
+    {
+        Task<JobApplicationDto?> Apply(Guid userId, ApplyJobDto request);
+        Task<List<JobApplicationDto>> GetMyApplications(Guid userId);
+    }
+}
